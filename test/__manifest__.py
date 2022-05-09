2c5db7c7ac17c85a67ab0ee4b48c08a51fa59114
Vlad Stefan Calapancea <vcalapancea@information-systems.ro>
2022-05-09 11:59:41 +0300

    vlad-test

---
+++ test/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "test",
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+
+    'installable': True,
+    'application': True,
+
+}
