eaa1e50e3bcf3ff285dfc0a9ccd833889d1ab742
Vlad Stefan Calapancea <vcalapancea@information-systems.ro>
2022-05-10 12:33:01 +0000

    New Module Odoo Space Mission

---
+++ odoo_space_mission/__manifest__.py
@@ -0,0 +1,19 @@
+{   
+    'name': "Odoo Space Mission",
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+   
+    'depends': ['base'],
+
+    
+    'data': [
+    ],
+
+    'demo': [
+    ],
+
+}
