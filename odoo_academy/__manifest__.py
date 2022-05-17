9f137f4b2e3b08f84d49407d3affd3e6578bc7ef
Vlad Stefan Calapancea <vcalapancea@information-systems.ro>
2022-05-17 15:45:27 +0300

    Odoo academy

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Odoo Academy",
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Something',
+    'version': '0.1',
+    'license': 'LGPL-3',
+
+    'depends': ['base', ],
+
+    'data': [
+    ],
+
+    'demo': [
+    ],
+}
